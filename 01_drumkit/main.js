88a6173f884d4812a091883643bfe4ae5d511a6e
mcrd25 <mayacrd25@gmail.com>
2021-01-08 14:48:37 -0400

    Completed day 1 and restructured the js code

--- 01_drumkit/main.js
+++ 01_drumkit/main.js
@@ -1,9 +1,17 @@
-window.addEventListener('keydown', (e) => {
-  console.log(e);
-  const keyCode = e.keyCode;
+const removeTransition = (e) => {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
+const playSound = (e) => {
+  const keyCode = e.keyCode
   const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
-  console.log(audio);
+  const key = document.querySelector(`div[data-key="${keyCode}"]`);
   if (!audio) return;
-  console.log(audio);
+  key.classList.add('playing');
+  audio.currentTime = 0;
   audio.play();
-});
+}
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
